f2d263960bbe6d8e7d7f490890cf5bd2637dbfe5
Luis Diego <lfdiego7@gmail.com>
2020-06-12 14:56:03 -0500

    feature: add animation menu

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 
@@ -23,29 +23,75 @@ const links = [
   },
 ]
 
-const Header = ({ siteTitle }) => (
-  <header>
-    <div className="header__inner">
-      <Link to="/" aria-label="Home" className="header__logo">
-        <span className="header__logo__mark">&gt;</span>
-        <span className="header__logo__text">/home</span>
-        <span className="header__logo__cursor"> </span>
-      </Link>
-      <div className="header__right">
+const Header = () => {
+  const [opened, setOpen] = useState(false)
+
+  const handleToogle = () => {
+    setOpen(!opened)
+  }
+
+  const closeMenu = () => {
+    setOpen(false)
+  }
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      if (opened) {
+        window.document.body.classList.add("no-scroll")
+      } else {
+        window.document.body.classList.remove("no-scroll")
+      }
+    }
+  }, [opened])
+
+  return (
+    <header>
+      <div className="header__inner">
+        <Link to="/" aria-label="Home" className="header__logo">
+          <span className="header__logo__mark">&gt;</span>
+          <span className="header__logo__text">/home</span>
+          <span className="header__logo__cursor"> </span>
+        </Link>
+        <div className="header__right">
+          {links.map((l, i) => (
+            <a
+              key={`header-link-${i}`}
+              href={l.to}
+              className="header__link"
+              arial-label={l.title}
+            >
+              {l.title}
+            </a>
+          ))}
+          <a
+            role="button"
+            className={opened ? "header__menu is-active" : "header__menu"}
+            aria-label="menu"
+            aria-expanded="false"
+            onClick={handleToogle}
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+      </div>
+      <div className={opened ? "header__mobile open" : "header__mobile"}>
         {links.map((l, i) => (
-          <Link
+          <a
             key={`header-link-${i}`}
-            to={l.to}
-            className="header__link"
+            href={l.to}
+            className="header__link mobile"
             arial-label={l.title}
+            onClick={closeMenu}
           >
             {l.title}
-          </Link>
+          </a>
         ))}
       </div>
-    </div>
-  </header>
-)
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
